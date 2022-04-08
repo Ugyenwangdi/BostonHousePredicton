73cc36c7011307c4026acb4b3a01b3d56a24a67d
Ugyenwangdi <57824653+Ugyenwangdi@users.noreply.github.com>
2022-04-08 10:29:19 +0600

    Update settings.py

--- housepriceprediction/settings.py
+++ housepriceprediction/settings.py
@@ -25,7 +25,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 SECRET_KEY = 'MY_API_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['housepprediction.herokuapp.com', 'localhost', '127.0.0.1'] 
 
