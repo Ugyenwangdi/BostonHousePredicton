579af2d2fa85b7abc613b3153287356d7f79859a
Ugyenwangdi <57824653+Ugyenwangdi@users.noreply.github.com>
2022-04-08 10:28:57 +0600

    Update settings.py

--- housepriceprediction/settings.py
+++ housepriceprediction/settings.py
@@ -22,7 +22,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b@unk*h0yxop8#6rc(ivame#6vt#flmp0lv=a)m%xvi@#puzde'
+SECRET_KEY = 'MY_API_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
